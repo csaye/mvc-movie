b6b48c66abaf678e947a753f05cf814b287631b8
Cooper Saye <coopersaye@gmail.com>
2020-10-29 11:06:54 -0700

    Implement rating property.

--- MvcMovie/Migrations/20201029180102_InitialCreate.Designer.cs
+++ MvcMovie/Migrations/20201029180102_InitialCreate.Designer.cs
@@ -9,7 +9,7 @@ using MvcMovie.Data;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20201027182740_InitialCreate")]
+    [Migration("20201029180102_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -28,6 +28,9 @@ namespace MvcMovie.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("ReleaseDate")
