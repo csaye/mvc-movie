fab808c215427f02ead12cdc7751c8f424fa5d9c
Cooper Saye <coopersaye@gmail.com>
2020-10-30 20:54:49 -0700

    Implement movie validation.

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -7,17 +7,28 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [StringLength(60)]
+        [Required]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
-        public string Genre { get; set; }
-
         [Column(TypeName = "decimal(18, 2)")]
+        [DataType(DataType.Currency)]
+        [Range(1, 100)]
         public decimal Price { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s-]*$")]
+        [StringLength(30)]
+        [Required]
+        public string Genre { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
     }
 }
